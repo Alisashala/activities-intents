61a0cdfe7595ea1ac349eb4d90eead9f4d825f7d
dean <nicklasfrederiksen@gmail.com>
2023-03-02 15:36:44 +0100

    not first

--- app/src/main/java/com/example/intents_example/ApprovalActivity.java
+++ app/src/main/java/com/example/intents_example/ApprovalActivity.java
@@ -2,17 +2,15 @@ package com.example.intents_example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 public class ApprovalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
+        setContentView(R.layout.activity_approval);
     }
 
     public void returnReply(View view){
