cfca176cee8890c52a51ff7084fc819bff9bcca6
dean <nicklasfrederiksen@gmail.com>
2023-03-02 14:42:48 +0100

    close to first

--- app/src/main/java/com/example/intents_example/RegistrationActivity.java
+++ app/src/main/java/com/example/intents_example/RegistrationActivity.java
@@ -1,18 +1,13 @@
 package com.example.intents_example;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class Registration extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
     }
-
-
 }
